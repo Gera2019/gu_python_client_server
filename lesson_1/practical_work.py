83b7a22c379697a0ce2bb68359d79dcf6034a3e5
Gera2019 <gumanoid2@gmail.com>
2022-02-11 01:43:05 +0300

    lesson_1 complete. Task_3 update

--- lesson_1/practical_work.py
+++ lesson_1/practical_work.py
@@ -8,25 +8,32 @@ words_u = ('\u0440\u0430\u0437\u0440\u0430\u0431\u043E\u0442\u043A\u0430',
            '\u0441\u043E\u043A\u0435\u0442',
            '\u0434\u0435\u043A\u043E\u0440\u0430\u0442\u043E\u0440')
 
-print('task1', *((item,type(item)) for item in words))
-print('task1', *((item,type(item)) for item in words_u))
+print('\n### Task 1 ###')
+print(*((item,type(item)) for item in words))
+print(*((item,type(item)) for item in words_u))
 
 ### 2.Каждое из слов «class», «function», «method» записать в байтовом типе без преобразования
 # в последовательность кодов (не используя методы encode и decode) и определить тип,
 # содержимое и длину соответствующих переменных.
 
 words_b = (b'class', b'function', b'method')
+
+print('\n### Task 2 ###')
 print('task2', *((item, type(item), len(item)) for item in words_b))
 
 
 ### 3. Определить, какие из слов «attribute», «класс», «функция», «type» невозможно записать в байтовом типе.
 
-w1 = b'attribute'
-# w2 = b'класс'
-## SyntaxError: bytes can only contain ASCII literal characters.
-# w3 = b'функция'
-## SyntaxError: bytes can only contain ASCII literal characters.
-w4 = b'type'
+words = ('attribute','класс','функция','type')
+
+isascii = lambda s: len(s) == len(s.encode())
+
+print('\n### Task 3 ###')
+for word in words:
+    if isascii(word):
+        print(f'The word "{word}" can be written in bytes')
+    else:
+        print(f'The word "{word}" cannot be written in bytes')
 
 ### 4. Преобразовать слова «разработка», «администрирование», «protocol», «standard» из строкового
 # представления в байтовое и выполнить обратное преобразование (используя методы encode и decode).
@@ -35,14 +42,16 @@ words = ('разработка', 'администрирование', 'protocol
 words_enc = [s.encode('utf-8') for s in words]
 words_dec = [bw.decode('utf-8') for bw in words_enc]
 
-print('task4 - words:', words, '\ntask4 - words encoded:', words_enc, '\ntask4 - words decoded:', words_dec)
+print('\n### Task 4 ###')
+print('words:', words, '\nwords encoded:', words_enc, '\nwords decoded:', words_dec)
 
 ### 5. Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты из байтовового в
 # строковый тип на кириллице.
 import locale
 import subprocess
 
-print('task5:', locale.getpreferredencoding())
+print('\n### Task 5 ###')
+print(locale.getpreferredencoding())
 
 results = ''
 
@@ -60,7 +69,7 @@ head_exec = subprocess.Popen(head_cmd, stdin=man_exec.stdout, stdout=subprocess.
 for line in head_exec.stdout:
     results += line.decode('utf-8')
 
-print('task5', results)
+print(results)
 
 ### 6. Создать текстовый файл test_file.txt, заполнить его тремя строками: «сетевое программирование», «сокет»,
 # «декоратор». Проверить кодировку файла по умолчанию. Принудительно открыть файл в формате Unicode и
@@ -68,8 +77,7 @@ print('task5', results)
 
 import chardet
 
-#
-print('###### task6 ######')
+print('\n### task6 ###')
 print('Default encoding: ', locale.getpreferredencoding())
 
 lines = ['сетевое программирование\n', 'сокет\n', 'декоратор\n']
@@ -82,8 +90,5 @@ with open  (path, encoding='windows-1251', mode='w') as f:
 with open(path, 'rb') as f:
     text = f.read()
     encoding = chardet.detect(text)['encoding']
-
-
-with open(path, encoding=encoding, errors='replace') as f:
-    print('File read: ', f.read())
+    print(text.decode(encoding))
 
