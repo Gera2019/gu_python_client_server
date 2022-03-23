e4c2ef2e8c8b076e3874f0c54da3897620ddb606
alexandragerasimova <gumanoid2@gmail.com>
2022-03-23 12:03:11 +0300

    lesson 7 completed

---
+++ lesson_7/common/variables.py
@@ -0,0 +1,21 @@
+import logging
+
+DEFAULT_ADDRESS = 'localhost'
+DEFAULT_PORT = 7777
+DEFAULT_MODE = 'listen'
+MSG_LENGTH = 5000
+ENCODING = 'utf-8'
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'mess_text'
+
+LOGGING_LEVEL = logging.DEBUG
+SENDER = 'sender'
